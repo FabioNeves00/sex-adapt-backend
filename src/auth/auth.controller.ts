6d7bb0af98f0eaf4fdd9ee5411ae1895e1cabddd
Fabio Neves <fabinhoneves09@gmail.com>
2023-03-07 15:22:47 -0300

    fix: removed override status code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -25,27 +25,23 @@ export class AuthController {
 
   @Public()
   @Post('local/signup')
-  @HttpCode(HttpStatus.CREATED)
   signup_local(@Body() createUserDto: CreateUserDto): Promise<Tokens> {
     return this.authService.signup_local(createUserDto);
   }
 
   @Public()
   @Post('local/signin')
-  @HttpCode(HttpStatus.OK)
   signin_local(@Body() authDto: AuthDto): Promise<Tokens> {
     return this.authService.signin_local(authDto);
   }
 
   @Post('logout')
-  @HttpCode(HttpStatus.OK)
   logout(@GetCurrentUserId() userId: string) {
     return this.authService.logout(userId);
   }
 
   @UseGuards(RefreshTokenGuard)
   @Post('refresh')
-  @HttpCode(HttpStatus.OK)
   refresh_token(
     @GetCurrentUserId() userId: string,
     @GetCurrentUser('refreshToken') refreshToken: string
