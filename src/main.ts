6a3e7025a72b020dc6db1b68285dd5e29ec9deab
Fabio Neves <fabinhoneves09@gmail.com>
2023-04-12 17:44:50 -0300

    chore: added swagger static files and loader

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import * as morgan from 'morgan';
 import { Request } from 'express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import { resolve } from 'path';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,7 +26,19 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('swagger', app, document);
+  if (process.env.NODE_ENV === 'development') {
+    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
+
+    // write swagger json file
+    const pathToSwaggerJson = resolve(
+      pathToSwaggerStaticFolder,
+      'swagger.json',
+    );
+    const swaggerJson = JSON.stringify(document, null, 2);
+    writeFileSync(pathToSwaggerJson, swaggerJson);
+    console.log(`Swagger JSON file written to: '/swagger-static/swagger.json'`);
+  }
   await app.listen(configService.get('PORT'), () => {
     console.log(`Listening on localhost:${configService.get('PORT')}`);
   });
